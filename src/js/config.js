09a12838387ed5e6b71c1ec1688a8ecd08c15c33
towhidnoor <tnailyrami@gmail.com>
2021-11-28 13:18:52 +0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// the file will contain varaible which will be responsible for defining some important
+// data for the application itself
+// the use of uppercase means the variable will never change, used mostly in config.js
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = 'c8a62876-af5d-4251-8585-2ab08668ae0e';
+export const MODAL_CLOSE_SEC = 2.5;
